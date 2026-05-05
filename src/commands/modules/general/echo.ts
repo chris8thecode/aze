899ef8017c28e87017fdba3f9329b30e3918f7f8
ni5arga <git@nisarga.me>
2026-05-05 21:30:20 +0530

    chore(echo): restrict to owner

--- src/commands/modules/general/echo.ts
+++ src/commands/modules/general/echo.ts
@@ -5,6 +5,7 @@ export default defineCommand({
   category: 'general',
   description: 'Repeat the given text back.',
   usage: 'echo <text>',
+  permission: 'owner',
   run: async ({ send, message, args }) => {
     if (args.length === 0) return send.reply(message, 'Usage: echo <text>')
     await send.reply(message, args.join(' '))
